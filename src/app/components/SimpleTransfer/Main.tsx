0738cc3071c49b5cab57df3bd533c0de257abcba
saik434455@gmail.com <saik434455@gmail.com>
2024-04-16 21:02:11 +0530

    final ui changes completed

--- src/app/components/SimpleTransfer/Main.tsx
+++ src/app/components/SimpleTransfer/Main.tsx
@@ -17,7 +17,27 @@ const Main = () => {
   const [contract, setContract] = useState<Web3.Contract | null>(null);
   const [address, setAddress] = useState<string>("");
   const [amount, setAmount] = useState<number>(0);
+  const [networkId, setNetworkId] = useState<BigInt | null>(null);
+  const [transactionStatus, setTransactionStatus] = useState<
+    "pending" | "success" | "error"
+  >("");
+  const [userBalance, setUserBalance] = useState<string>("");
 
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout> | null = null;
+
+    if (transactionStatus === "success" || transactionStatus === "error") {
+      timer = setTimeout(() => {
+        setTransactionStatus("");
+      }, 1000);
+    }
+
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [transactionStatus]);
   useEffect(() => {
     const initialize = async () => {
       if (typeof window.ethereum !== "undefined" && window.ethereum) {
@@ -26,7 +46,16 @@ const Main = () => {
         try {
           await window.ethereum.enable();
           const currentNetworkId = await web3Instance.eth.net.getId();
+          const accounts = await web3Instance.eth.getAccounts();
+          const balance = await web3Instance.eth.getBalance(accounts[0]);
+          const formattedBalance = parseFloat(
+            web3Instance.utils.fromWei(balance, "ether")
+          ).toFixed(2);
+          setUserBalance(formattedBalance);
+
+          console.log("Current Network ID:", currentNetworkId);
           const networkId = BigInt(31337); // Convert 31337 to BigInt
+          setNetworkId(networkId); // Update the network ID state
           if (currentNetworkId === networkId) {
             const contractInstance = new web3Instance.eth.Contract(
               SimpleABI,
@@ -71,16 +100,19 @@ const Main = () => {
         result = await contract.methods[functionName](...params).call();
       } else {
         console.log("Sending transaction...");
+        setTransactionStatus("pending");
         result = await contract.methods[functionName](...params).send({
           from: window.ethereum.selectedAddress,
           value: web3.utils.toWei(msgValue.toString(), "ether"),
           gas: 41000,
         });
+        setTransactionStatus("success");
       }
       console.log("Transaction result:", result);
       setError(""); // Clear error state on successful call
     } catch (error) {
       console.log("Error calling contract function:", error);
+      setTransactionStatus("error");
       setError("An error occurred while calling the contract function.");
     }
   };
@@ -113,6 +145,15 @@ const Main = () => {
             Wallet: {walletAddress}
           </p>
         )}
+        {networkId && (
+          <p className="text-xl font-semibold text-violet-700 bg-white px-4 py-2 rounded-xl">
+            Network ID: {networkId.toString()}
+          </p>
+        )}
+        <div className="text-green-600 font-semibold bg-white p-4 rounded-lg">
+          Balance: {userBalance} ETH
+        </div>
+
         <WalletConnection
           connected={connected}
           handleConnectWallet={handleConnectWallet}
@@ -143,6 +184,65 @@ const Main = () => {
           Send
         </button>
       </div>
+      {transactionStatus === "pending" && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="bg-black p-6 rounded-lg shadow-md">
+              <div className="flex justify-center items-center">
+                <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-violet-700"></div>
+                <p className="ml-4">Transaction in progress...</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {transactionStatus === "success" && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="bg-black p-6 rounded-lg shadow-md">
+              <div className="flex justify-center items-center">
+                <svg
+                  className="h-8 w-8 text-green-500"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
+                  <polyline points="22 4 12 14.01 9 11.01" />
+                </svg>
+                <p className="ml-4">Transaction successful!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {transactionStatus === "error" && (
+        <div className="fixed z-10 inset-0 overflow-y-auto">
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="bg-black p-6 rounded-lg shadow-md">
+              <div className="flex justify-center items-center">
+                <svg
+                  className="h-8 w-8 text-red-500"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <circle cx="12" cy="12" r="10" />
+                  <line x1="12" y1="8" x2="12" y2="12" />
+                  <line x1="12" y1="16" x2="12.01" y2="16" />
+                </svg>
+                <p className="ml-4">Transaction failed.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
